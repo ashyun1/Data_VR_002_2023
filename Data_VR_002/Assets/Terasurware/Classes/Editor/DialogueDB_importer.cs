f214e5c3ae16d051f0e7287d0a0d7be1e1ae3357
ashyun1 <ashyun5490@naver.com>
2023-04-27 11:58:02 +0900

    다디얼로그 DB 까지

---
+++ Data_VR_002/Assets/Terasurware/Classes/Editor/DialogueDB_importer.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+using System.Xml.Serialization;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+
+public class DialogueDB_importer : AssetPostprocessor {
+	private static readonly string filePath = "Assets/Resources/ExcelDB/DialogueDB.xlsx";
+	private static readonly string exportPath = "Assets/Resources/ExcelDB/DialogueDB.asset";
+	private static readonly string[] sheetNames = {  };
+	
+	static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+	{
+		foreach (string asset in importedAssets) {
+			if (!filePath.Equals (asset))
+				continue;
+				
+			Entity_Dialogue data = (Entity_Dialogue)AssetDatabase.LoadAssetAtPath (exportPath, typeof(Entity_Dialogue));
+			if (data == null) {
+				data = ScriptableObject.CreateInstance<Entity_Dialogue> ();
+				AssetDatabase.CreateAsset ((ScriptableObject)data, exportPath);
+				data.hideFlags = HideFlags.NotEditable;
+			}
+			
+			data.sheets.Clear ();
+			using (FileStream stream = File.Open (filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) {
+				IWorkbook book = null;
+				if (Path.GetExtension (filePath) == ".xls") {
+					book = new HSSFWorkbook(stream);
+				} else {
+					book = new XSSFWorkbook(stream);
+				}
+				
+				foreach(string sheetName in sheetNames) {
+					ISheet sheet = book.GetSheet(sheetName);
+					if( sheet == null ) {
+						Debug.LogError("[QuestData] sheet not found:" + sheetName);
+						continue;
+					}
+
+					Entity_Dialogue.Sheet s = new Entity_Dialogue.Sheet ();
+					s.name = sheetName;
+				
+					for (int i=1; i<= sheet.LastRowNum; i++) {
+						IRow row = sheet.GetRow (i);
+						ICell cell = null;
+						
+						Entity_Dialogue.Param p = new Entity_Dialogue.Param ();
+						
+						s.list.Add (p);
+					}
+					data.sheets.Add(s);
+				}
+			}
+
+			ScriptableObject obj = AssetDatabase.LoadAssetAtPath (exportPath, typeof(ScriptableObject)) as ScriptableObject;
+			EditorUtility.SetDirty (obj);
+		}
+	}
+}
